4d59d753fb81734153ec650c0d6e4af6844aa9ce
Iain Bromley <iaineubromley@gmail.com>
2019-09-13 23:49:18 -0700

    Adds XML -> JSON conversion for S3 storage
    Adds additional dependencies on xmljson libraries

--- scraper.py
+++ scraper.py
@@ -1,12 +1,19 @@
 import requests 
 import configparser
-import xml.etree.ElementTree as ET
 import boto3
+from datetime import datetime
 
+# XML -> JSON parsing
+from xmljson import parker as bf
+from xml.etree.ElementTree import fromstring
+from json import dumps
+
+# Read configuration item
 config = configparser.ConfigParser()
 config.read('api.cfg')
 AWS_KEY = config.get('AWS', 'KEY')
 SECRET = config.get('AWS', 'SECRET')
+BUCKET = config.get('AWS', 'BUCKET')
 API_KEY = config.get("OBA","API_KEY")
 STOP_ID = config.get("OBA","STOP_ID")
 
@@ -14,11 +21,15 @@ STOP_ID = config.get("OBA","STOP_ID")
 url = 'http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/' + STOP_ID + '.xml?key=' + API_KEY
 
 r = requests.get(url)
-tree = ET.parse(r.text)
-root = tree.getroot()
 
 s3 = boto3.client('s3',
                     region_name="us-west-2",
                     aws_access_key_id=AWS_KEY,
                     aws_secret_access_key=SECRET
                     )
+
+json_object = dumps(bf.data(fromstring(r.text)))
+
+object_name = STOP_ID + '/' + datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.json'
+
+s3.put_object(Body=json_object, Bucket=BUCKET, Key=object_name)
